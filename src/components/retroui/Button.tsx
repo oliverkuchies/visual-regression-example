9aad7c9ee75ea781bfcb0611fc15d587ce7126a3
Ari Perkkiö <ari.perkkio@gmail.com>
2026-06-17 09:30:28 +0300

    fix: change #3 to get enough builds for turbosnap

--- src/components/retroui/Button.tsx
+++ src/components/retroui/Button.tsx
@@ -4,7 +4,7 @@ import React, { ButtonHTMLAttributes } from "react";
 import { Slot } from "@radix-ui/react-slot";
 
 export const buttonVariants = cva(
-  "font-head transition-all rounded outline-hidden cursor-pointer duration-200 font-medium flex items-center",
+  "font-head transition-all rounded outline-hidden cursor-pointer duration-200 font-medium",
   {
     variants: {
       variant: {
@@ -15,7 +15,7 @@ export const buttonVariants = cva(
         outline:
           "shadow-md hover:shadow active:shadow-none bg-transparent border-2 transition hover:translate-y-1 active:translate-y-2 active:translate-x-1",
         link: "bg-transparent hover:underline",
-        ghost: "bg-transparent hover:bg-accent",
+        ghost: "bg-transparent hover:bg-accent"
       },
       size: {
         sm: "px-3 py-1 text-sm shadow hover:shadow-none",
@@ -32,8 +32,7 @@ export const buttonVariants = cva(
 );
 
 export interface IButtonProps
-  extends
-    ButtonHTMLAttributes<HTMLButtonElement>,
+  extends ButtonHTMLAttributes<HTMLButtonElement>,
     VariantProps<typeof buttonVariants> {
   asChild?: boolean;
 }
